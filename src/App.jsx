0575d6c87044c84623cc2c3f612851bffe516a09
Sadavarsh Saini <92135085+Sadavarsh@users.noreply.github.com>
2024-05-27 17:57:21 +0530

    update gpt3 app code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import {Footer, Blog, Features, Header, Possibility, WhatGPT3} from "./containers";
+import {Cta, Brand,  Navbar } from "./components";
+import "./App.css";
 function App() {
-
-
   return (
-    <>
-     <p>Hello World</p>
-    </>
+   <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <Cta />
+    <Blog />
+    <Footer />
+   </div>
   )
 }
 
--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,3 +1,4 @@
+import "./article.css";
 
 const Article = () => {
   return (
